36cd7b977fd3de0b74daca9a0eab44158364ca6c
Josselin <robinjosselin5@gmail.com>
2026-06-15 16:59:20 +0200

    Init react rooter - like a blanck project

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,50 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Link, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-    <App />,
-)
+// page components
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// router creation
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <Home />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <About />
+        </main>
+      </>
+    ),
+  },
+]);
+
+// rendering
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+}
